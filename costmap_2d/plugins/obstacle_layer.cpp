d69140f9aa09653a7dbc48ef40458cc9058bd890
Ferd Chen <freason@yeah.net>
2018-09-08 20:50:58 +0800

    treat zero ray distance as infinite

--- costmap_2d/plugins/obstacle_layer.cpp
+++ costmap_2d/plugins/obstacle_layer.cpp
@@ -283,7 +283,7 @@ void ObstacleLayer::laserScanValidInfCallback(const sensor_msgs::LaserScanConstP
   for (size_t i = 0; i < message.ranges.size(); i++)
   {
     float range = message.ranges[ i ];
-    if (!std::isfinite(range) && range > 0)
+    if ((!std::isfinite(range) && range > 0) || range == 0.0)
     {
       message.ranges[ i ] = message.range_max - epsilon;
     }
